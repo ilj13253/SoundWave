bd464373fed5dcf04ba5ea1472df76412253a4a2
nero ir <butackovilj25@gmail.com>
2024-09-30 01:03:46 +0800

    GGG

---
+++ SoundWave-master/Model.SoundWave/Entities/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace Model.SoundWave.Entities;
+
+public class Album
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string UrlPreviewImg { get; set; }
+    public required List<Song> Songs { get; set; }
+    public required List<Group> Groups { get; set; }
+   // public required decimal? Price{ get; set; }
+}
